467227387b1f3beb96151056e4737767ae2f1739
maharanidiradinova <114593041+maharanidiradinova@users.noreply.github.com>
2023-01-08 13:48:00 +0700

    Add files via upload

---
+++ cetakargumen.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Maharani2101092052_modul8;
+
+/**
+ *
+ * @author Maharani Diradinova
+ */
+public class cetakargumen {
+    public static void main(String[] args)
+    {
+        
+        //arguments = java Hello world that is all
+               
+        int i;
+        for(i=0;i<args.length;i++)
+        {
+            System.out.println(args[i]);
+        }
+    }
+
+}
